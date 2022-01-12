08f8712b0c36b8fcc2d712a78977927f5ed2a391
tejaswilkhoo <tejaswilkhoo@hotmail.com>
2022-01-12 02:02:33 -0500

    working game without styles

--- memory-match/src/App.js
+++ memory-match/src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-    {"src": "/img/helmet-1.png"},
-    {"src": "/img/potion-1.png"},
-    {"src": "/img/ring-1.png"},
-    {"src": "/img/scroll-1.png"},
-    {"src": "/img/shield-1.png"},
-    {"src": "/img/sword-1.png"}
+    {"src": "/img/helmet-1.png", matched: false},
+    {"src": "/img/potion-1.png", matched: false},
+    {"src": "/img/ring-1.png", matched: false},
+    {"src": "/img/scroll-1.png", matched: false},
+    {"src": "/img/shield-1.png", matched: false},
+    {"src": "/img/sword-1.png", matched: false}
 ]
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
     const [turns, setTurns] = useState(0);
     const [choiceOne, setChoiceOne] = useState(null);
     const [choiceTwo, setChoiceTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
     
 
     // shuffle cards
@@ -37,37 +38,63 @@ function App() {
 
     // compare the two cards, note that useEffect will fire every time anything in the dependency array changes
     useEffect(() => {
+
         if (choiceOne && choiceTwo) {
+            setDisabled(true);
             if (choiceOne.src === choiceTwo.src) {
-                console.log("match")
+
+                setCards(prevCards => {
+                    return prevCards.map((card) => {
+                        if (card.src === choiceOne.src) {
+                            return {...card, matched: true}
+                        } else {
+                            return card
+                        }
+                    })
+                })
+
                 resetTurn()
             } else {
-                console.log("no match")
-                resetTurn()
+                setTimeout(() => resetTurn(), 1000)
             }
             
         }
     }, [choiceOne, choiceTwo])
+
+    console.log(cards)
 
     // reset the cards
     const resetTurn = () => {
         setChoiceOne(null)
         setChoiceTwo(null)
         setTurns(prevTurns => prevTurns + 1)
+        setDisabled(false)
     }
 
-  return (
-    <div className="App">
-        <h1>Magic Match</h1>
-        <button onClick={shuffleCards}>New Game</button>
+    // start a new game automatically
+    useEffect(() => {
+        shuffleCards()
+    }, [])
 
-        <div className="card-grid">
-            {cards.map((card) => (
-                <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
-            ))}
+    return (
+        <div className="App">
+            <h1>Magic Match</h1>
+            <button onClick={shuffleCards}>New Game</button>
+
+            <div className="card-grid">
+                {cards.map((card) => (
+                    <SingleCard 
+                        key={card.id} 
+                        card={card} 
+                        handleChoice={handleChoice} 
+                        flipped={card === choiceOne || card === choiceTwo || card.matched}
+                        disabled={disabled}
+                    />
+                ))}
+            </div>
+            <p>Turns: {turns}</p>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- memory-match/src/components/SingleCard.js
+++ memory-match/src/components/SingleCard.js
@@ -1,14 +1,16 @@
 import './SingleCard.css'
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
     const handleClick = () => {
-        handleChoice(card)
+        if (!disabled) {
+            handleChoice(card)
+        }
     }
 
     return (
         <div className="card">
-            <div>
+            <div className={flipped ? "flipped" : ""}>
                 <img src={card.src} className="front" alt="front of the card" />
                 <img src="/img/cover.png" className="back" onClick={handleClick} alt="back of the card"/>
             </div>
